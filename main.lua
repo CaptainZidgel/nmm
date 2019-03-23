69f608dbd1ccd11c23b4cdc02bd211ae2ea8819b
Michael <thesentientgelatin@gmail.com>
2019-03-22 20:54:09 -0700

    updated main.lua
    Added ability to remove an enemy token when you make a mill, but there is a glitch. Old mills will count as being a new mill, forcing the player to remove an enemy's token on any turn.

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 function love.load()
+	mills = {}
 	board = love.graphics.newImage('gfx/nmm_board.png')
 	pieces = {}
 	phase = 1 -- phases are (1) placing pieces, (2) moving placed pieces, (3) Flying- for more info see the wikipedia page for NMM
@@ -43,74 +44,63 @@ function love.load()
 		{x = 300+334, y = 652, id = 23, w = 32, h = 32, resident = "neutral", cons = {20, 22, 24}},
 		{x = 300+652, y = 652, id = 24, w = 32, h = 32, resident = "neutral", cons = {15, 23}}
 	}
-	
+	inbank = 18
+	needToRemove = 0
 end
 
 function love.update(dt)
+	print(needToRemove)
 	mx, my = love.mouse.getPosition()
-	local inbank = 0
 	for i,v in ipairs(pieces) do
 		if v.state == "moving" then
 			v.x = mx
 			v.y = my
 		end
-		
-		if v.placement ~= "bank" and phase == 1 then
-			inbank = inbank + 1
-			if inbank == 18 then
-				phase = 2
-				print("Phasechanged")
-			end
-		end
-		
 	end
-	
-	if love.keyboard.isDown("space") then
-		print("  ")
-	end
-	
+end
+
+function checkForMills()
 	if spots[1].resident == spots[2].resident and spots[1].resident == spots[3].resident and spots[1].resident ~= "neutral" then
-		createdMill(spots[1].resident)
+		createdMill(spots[1].resident, {1, 2, 3}) --6
 	elseif spots[1].resident == spots[10].resident and spots[1].resident == spots[22].resident and spots[1].resident ~= "neutral" then
-		createdMill(spots[1].resident)
+		createdMill(spots[1].resident, {1, 10, 22}) --220
 	elseif spots[4].resident == spots[5].resident and spots[4].resident == spots[6].resident and spots[4].resident ~= "neutral" then
-		createdMill(spots[4].resident)
+		createdMill(spots[4].resident, {4, 5, 6}) --120
 	elseif spots[7].resident == spots[8].resident and spots[7].resident == spots[9].resident and spots[7].resident ~= "neutral" then
-		createdMill(spots[7].resident)
+		createdMill(spots[7].resident, {7, 8, 9}) --504
 	elseif spots[11].resident == spots[10].resident and spots[11].resident == spots[12].resident and spots[10].resident ~= "neutral" then
-		createdMill(spots[11].resident)
+		createdMill(spots[11].resident, {10, 11, 12}) --1320
 	elseif spots[13].resident == spots[14].resident and spots[13].resident == spots[15].resident and spots[13].resident ~= "neutral" then
-		createdMill(spots[13].resident)
+		createdMill(spots[13].resident, {13, 14, 15}) --2730
 	elseif spots[4].resident == spots[11].resident and spots[4].resident == spots[19].resident and spots[4].resident ~= "neutral" then
-		createdMill(spots[4].resident)
+		createdMill(spots[4].resident, {4, 11, 19}) --836
 	elseif spots[3].resident == spots[15].resident and spots[3].resident == spots[24].resident and spots[3].resident ~= "neutral" then
-		createdMill(spots[3].resident)
+		createdMill(spots[3].resident, {3, 15, 24})
 	elseif spots[22].resident == spots[23].resident and spots[22].resident == spots[24].resident and spots[22].resident ~= "neutral" then
-		createdMill(spots[22].resident)
+		createdMill(spots[22].resident, {22, 23, 24})
 	elseif spots[6].resident == spots[14].resident and spots[6].resident == spots[21].resident and spots[6].resident ~= "neutral" then
-		createdMill(spots[6].resident)
+		createdMill(spots[6].resident, {6, 14, 21})
 	elseif spots[19].resident == spots[20].resident and spots[19].resident == spots[21].resident and spots[19].resident ~= "neutral" then
-		createdMill(spots[19].resident)
+		createdMill(spots[19].resident, {19, 20, 21})
 	elseif spots[16].resident == spots[17].resident and spots[16].resident == spots[18].resident and spots[16].resident ~= "neutral" then
-		createdMill(spots[16].resident)
+		createdMill(spots[16].resident, {16, 17, 18})
 	elseif spots[17].resident == spots[20].resident and spots[17].resident == spots[23].resident and spots[17].resident ~= "neutral" then
-		createdMill(spots[17].resident)
+		createdMill(spots[17].resident, {17, 20, 23})
 	elseif spots[2].resident == spots[5].resident and spots[2].resident == spots[8].resident and spots[2].resident ~= "neutral" then
-		createdMill(spots[2].resident)
+		createdMill(spots[2].resident, {2, 5, 8}) --80
 	elseif spots[1].resident == spots[4].resident and spots[1].resident == spots[7].resident and spots[1].resident ~= "neutral" then
-		createdMill(spots[1].resident)
+		createdMill(spots[1].resident, {1, 4, 7}) --28
 	elseif spots[3].resident == spots[6].resident and spots[3].resident == spots[9].resident and spots[3].resident ~= "neutral" then
-		createdMill(spots[3].resident)
+		createdMill(spots[3].resident, {3, 6, 9}) --162
 	elseif spots[16].resident == spots[19].resident and spots[16].resident == spots[22].resident and spots[16].resident ~= "neutral" then
-		createdMill(spots[16].resident)
+		createdMill(spots[16].resident, {16, 19, 22})
 	elseif spots[18].resident == spots[21].resident and spots[18].resident == spots[24].resident and spots[18].resident ~= "neutral" then
-		createdMill(spots[18].resident)
+		createdMill(spots[18].resident, {18, 21, 24})
 	elseif spots[7].resident == spots[12].resident and spots[7].resident == spots[16].resident and spots[7].resident ~= "neutral" then
-		createdMill(spots[7].resident)
+		createdMill(spots[7].resident, {7, 12, 16}) --1344
 	elseif spots[9].resident == spots[13].resident and spots[9].resident == spots[18].resident and spots[9].resident ~= "neutral" then
-		createdMill(spots[9].resident)
+		createdMill(spots[9].resident, {9, 13, 18}) --2106
 	end
-	
 end
 
 function flipturn()
@@ -121,13 +111,37 @@ function flipturn()
 	end
 end
 
-function createdMill(team)
+function createdMill(team, millspots)
 	local otherteam = "blue"
 	--if team == "blue" then otherteam = "red" end
 	--print("Player " .. team .. " created a mill! They get to remove one of " .. otherteam .. "'s tokens!")
+	local contains = false
+	for i,v in ipairs(mills) do
+		if v == millspots then
+			contains = true
+		end
+	end
+	if contains == true then
+		print("")
+	else
+		needToRemove = 1
+		--mills[millspots[1] * millspots[2] * millspots[3]] = millspots
+		table.insert(mills, millspots)
+	end
 end
 
 function love.mousepressed(mousex, mousey)
+	if needToRemove == 1 then
+		for i,v in ipairs(pieces) do
+			if mousex < v.x + v.r and mousex > v.x - v.r and mousey < v.y + v.r and mousey > v.y - v.r and pointerCarrying == "nothing" and turn ~= v.owner then
+				if v.state == "static" and v.placement ~= "bank" then
+					v.placement = "gone"
+					needToRemove = 0
+					flipturn()
+				end
+			end
+		end
+	end
 	if phase == 1 then
 		if pointerCarrying == "nothing" then
 			for i,v in ipairs(pieces) do
@@ -148,8 +162,15 @@ function love.mousepressed(mousex, mousey)
 					v.state = "static"
 					v.placement = b.id
 					b.resident = v.owner
-					flipturn()
 					pointerCarrying = "nothing"
+					checkForMills()
+					inbank = inbank - 1
+					if inbank == 0 then
+						phase = 2
+					end
+					if needToRemove == 0 then
+						flipturn()
+					end
 				end
 			end
 		end
@@ -175,7 +196,8 @@ function love.mousepressed(mousex, mousey)
 							v.placement = b.id
 							b.resident = v.owner
 							pointerCarrying = "nothing"
-							if value == b.id then
+							checkForMills()
+							if value == b.id and needToRemove == 0 then
 								flipturn()
 							end
 						end
@@ -191,12 +213,14 @@ function love.draw()
 	love.graphics.setColor(1, 1, 1)					--color to white
 	love.graphics.draw(board, 300, 0)				--draw board
 	for i,v in ipairs(pieces) do
-		if v.owner == "red" then
-			love.graphics.setColor(0.827, 0.235, 0.258)
-			love.graphics.circle("fill", v.x, v.y, v.r)
-		else
-			love.graphics.setColor(0.235, 0.564, 0.827)
-			love.graphics.circle("fill", v.x, v.y, v.r)
+		if v.placement ~= "gone" then
+			if v.owner == "red" then
+				love.graphics.setColor(0.827, 0.235, 0.258)
+				love.graphics.circle("fill", v.x, v.y, v.r)
+			else
+				love.graphics.setColor(0.235, 0.564, 0.827)
+				love.graphics.circle("fill", v.x, v.y, v.r)
+			end
 		end
 	end
 	--[[for i,v in ipairs(spots) do						--draws squares around the spots tokens are placable
